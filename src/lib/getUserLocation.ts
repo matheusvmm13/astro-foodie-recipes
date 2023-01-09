4bd82996550beee94af8e886a861af94c3b8dec4
Matheus Martins <matheus.martins@softonic.com>
2023-01-09 19:08:50 +0100

    fix: add env file to debug

--- src/lib/getUserLocation.ts
+++ src/lib/getUserLocation.ts
@@ -1,6 +1,6 @@
 export const getUserLocation = async () => {
     try {
-        const response: Response = await fetch(import.meta.env.PUBLIC_GEOAPIFY_API_KEY, { method: 'GET' });
+        const response: Response = await fetch('https://api.geoapify.com/v1/ipinfo?&apiKey=9d92bf811c2d4a569c3403f33637a2fa', { method: 'GET' });
         const location = await response.json();
         return location;
     } catch (error) {
