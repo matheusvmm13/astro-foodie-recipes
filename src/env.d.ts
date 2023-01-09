468d919d05e7d43e3b1516b4d33193406c5d72a6
Matheus Martins <matheus.martins@softonic.com>
2023-01-09 12:53:24 +0100

    fix: esbuild & typescritp errors

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="astro/client" />
+interface ImportMetaEnv {
+    readonly PUBLIC_GEOAPIFY_API_KEY: string;
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
--- src/lib/getUserLocation.ts
+++ src/lib/getUserLocation.ts
@@ -1,7 +1,7 @@
 export const getUserLocation = async () => {
     try {
-        const response: Response = await fetch('https://api.geoapify.com/v1/ipinfo?&apiKey=9d92bf811c2d4a569c3403f33637a2fa', { method: 'GET' });
-        const location: any = await response.json();
+        const response: Response = await fetch(import.meta.env.PUBLIC_GEOAPIFY_API_KEY, { method: 'GET' });
+        const location: unknown = await response.json();
         return location;
     } catch (error) {
         console.error('Couldnt get user location: ', error);
