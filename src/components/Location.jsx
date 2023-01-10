52634f6c0df58afebe0730bdb0642377a5a5ea68
Matheus Martins <matheus.martins@softonic.com>
2023-01-10 17:48:14 +0100

    fix: refactor Header component

--- src/components/Location.jsx
+++
@@ -1,9 +0,0 @@
-export const Location = async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1', { method: 'GET' });
-    const toDoNote = await response.json();
-    console.log('note', toDoNote);
-      
-    return (
-        <p>vegetables and fruits in {toDoNote.title}</p>
-    );
-};
