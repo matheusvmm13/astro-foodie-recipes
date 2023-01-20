765d9a6fe691c8cf78540689a5947a70089e8131
Matheus Martins <matheus.martins@softonic.com>
2023-01-20 13:58:15 +0100

    refactor: remove SSR config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,6 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
 import preact from "@astrojs/preact";
 
-// https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [preact()],
-  output: "server",
-  adapter: vercel()
 });
