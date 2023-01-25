7da5a0f04266366954ceddc7ba463d2698a8b5bb
Matheus Martins <matheus.martins@softonic.com>
2023-01-25 14:08:50 +0100

    feat: add api key type

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="astro/client" />
 interface ImportMetaEnv {
     readonly PUBLIC_GEOAPIFY_API_KEY: string;
+    readonly PUBLIC_OPENCAGEDATA_API_KEY: string;
   }
   
   interface ImportMeta {
