d9bed9ae2209f816f01cfb62c6a8730f398e1d98
Matheus Martins <matheus.martins@softonic.com>
2023-01-25 14:02:50 +0100

    feat: add cache to custom hook

--- src/hooks/useGeoLocation.hooks.jsx
+++ src/hooks/useGeoLocation.hooks.jsx
@@ -1,19 +1,29 @@
 import { useEffect, useState } from "preact/hooks";
 
-export const useGeoLocation = () => {
-    const [country, setCountry] = useState("...");
+let CACHE = "";
 
-    const successfulLookup = async (position) => {
-        const { latitude, longitude } = position.coords;
-        
-        const response = await fetch(
-            `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${import.meta.env.PUBLIC_OPENCAGEDATA_API_KEY}`
-        );
-        const userLocation = await response.json();
-        setCountry(userLocation.results[0]?.components?.country);
-    };
+export const useGeoLocation = () => {
+    const [country, setCountry] = useState("");
 
     useEffect(() => {
+        const successfulLookup = async (position) => {
+            const { latitude, longitude } = position.coords;
+    
+            if (CACHE.length > 3) {
+                setCountry(CACHE);
+                console.log("taking from cached country", CACHE);
+                return CACHE;
+            };
+    
+            const response = await fetch(
+                `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${import.meta.env.PUBLIC_OPENCAGEDATA_API_KEY}`
+            );
+            const userLocation = await response.json();
+            setCountry(userLocation.results[0]?.components?.country);
+            CACHE = country;
+            console.log("cache value:", CACHE);
+        };
+
         if (window.navigator.geolocation) {
             window.navigator.geolocation.getCurrentPosition(
                 successfulLookup,
@@ -22,7 +32,7 @@ export const useGeoLocation = () => {
         } else {
             console.warn("Geolocation is NOT available");
         }
-    }, []);
-    
+    }, [country]);
+
     return country;
 };
