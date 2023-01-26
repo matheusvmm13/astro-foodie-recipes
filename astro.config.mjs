1cc0b614a3efd8c9f354a60b9a0fe3ccad61a00c
Matheus Martins <matheus.martins@softonic.com>
2023-01-26 12:10:15 +0100

    feat: remove PWA config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,15 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
-import AstroPWA from "@vite-pwa/astro";
 
+// https://astro.build/config
 export default defineConfig({
+    vite: {
+        logLevel: "info",
+        define: {
+            __DATE__: `'${new Date().toISOString()}'`,
+        },
+    },
     integrations: [
         preact(),
-        AstroPWA({
-            mode: "development",
-            base: "/",
-            scope: "/",
-            includeAssets: ["favicon.ico"],
-            registerType: "autoUpdate",
-            manifest: {
-                name: "Astro PWA",
-                short_name: "Astro PWA",
-                theme_color: "#ffffff",
-            },
-            workbox: {
-                globPatterns: ["**/*.{css,js,html,svg,png,ico,txt}"],
-            },
-            devOptions: {
-                enabled: true,
-                navigateFallback: "/404",
-            },
-        }),
     ],
 });
